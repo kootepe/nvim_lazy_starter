5edc9f1ce76fa7431b66dc20980c87c04e41415c
Eero <eeero93@gmail.com>
2023-10-23 10:57:39 +0300

    Disable hlargs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,5 @@ return {
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "navarasu/onedark.nvim", enabled = true },
   { "olimorris/onedarkpro.nvim", enabled = false },
+  { "m-demare/hlargs.nvim", enabled = false },
 }
