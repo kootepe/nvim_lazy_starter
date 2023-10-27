355dd4c77bac0c729b70f1cbdd27f6e1a5e2064d
Eero <eeero93@gmail.com>
2023-10-28 00:27:04 +0300

    Add vimtex_indent_lists to prevent list indetation
    Doesnt seem to help

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,6 +1,7 @@
 local o = vim.o
+local g = vim.g
 local map = vim.keymap.set
 map("n", "<leader>vc", "<Plug>(vimtex-compile)", { desc = "Vimtex Compile" })
 map("n", "<leader>vv", "<Plug>(vimtex-view)", { desc = "Vimtex View" })
 
--- o.noautoindent = true
+g.vimtex_indent_lists = {}
